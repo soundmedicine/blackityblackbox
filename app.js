83ef5a90d51e0badb63c3327466fe13c8f7ddd5a
Landon Folkers <32934735+landonfolkers@users.noreply.github.com>
2018-01-19 14:15:37 -0700

    made changes

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 var textEntry = document.getElementById("text")
+var button = document.getElementsByID("Submit")
+
+
+button.addEventlistener("click", show)
+
+function show() {
+document.getElementById("front").innerHTML = window.location.search
+}
